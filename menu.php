d3c853187237f9576c10db8f51f871a9c245b7ec
pospak <pospk65@gmail.com>
2025-06-11 22:05:58 +0200

    úpravy

--- menu.php
+++ menu.php
@@ -1,7 +1,23 @@
 <header>
 <h1 class="full">Informační systém Vysoké školy polytechnické Jihlava</h1>
 <h1 class="short">IS VŠPJ</h1>
-<nav>
+<nav class="full">
+    <ul>
+        <li><a href="/">Úvod</a></li>
+        <li><a href="#">Osobní</a></li>
+        <li><a href="#">Výuka</a></li>
+        <li><a href="#">Studium</a></li>
+        <li><a href="#">Stipendia</a></li>
+        <li><a href="#">Učitelé</a></li>
+        <li><a href="#">Rozvrh</a></li>
+        <li><a href="/student/szz">SZZ</a></li>
+        <li><a href="#">Závěrečné práce</a></li>
+        <li><a href="/koleje/prehled-zadosti">Koleje</a></li>
+        <li><a href="#">Ostatní</a></li>
+        
+    </ul>
+</nav>
+<nav class="short">
     <ul>
         <li><a href="/">Úvod</a></li>
         <li><a href="#">Osobní</a></li>
