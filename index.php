355821d9032110342887be00fd0c1ee21300e925
pospak <pospk65@gmail.com>
2025-06-29 20:06:23 +0200

    úvodní stránka

--- index.php
+++ index.php
@@ -13,6 +13,22 @@
         <?php include "menu.php"?>
     <main>
         <h1>Úvod</h1>
+        <div class="container">
+  <div class="timetable">
+    <h1>
+        Zde bude viditelný rozvrh studenta
+    </h1>
+  </div>
+  <div class="messsages"><h1>
+    Zde budou viditelné Zprávy v IS
+  </h1></div>
+  <div class="credits">
+    <h1>Zde budou viditelné kredity studenta</h1>
+  </div>
+  <div class="opportunities">
+    <h1>Zde budou viditelné aktuální nabídky práce</h1>
+  </div>
+</div>
     </main>
     </div>
     
