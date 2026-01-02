f860fe155ca76104d96d42bb3c5da712fdea6482
pospak <pospk65@gmail.com>
2026-01-02 13:37:00 +0100

    wrong database connection delete

--- controlers/MainControler.php
+++ controlers/MainControler.php
@@ -13,15 +13,15 @@ class MainControler extends Controler
 
         $this->view = "main";
 
-        $connect = mysqli_connect("","us011225","Skorpik444","db011225");
-        $query = "SELECT * FROM posts WHERE public = 1 ORDER BY date DESC";
-        $merge = mysqli_query($connect,$query);
-        $results = mysqli_fetch_all($merge, MYSQLI_ASSOC);
+        // $connect = mysqli_connect("","us011225","Skorpik444","db011225");
+        // $query = "SELECT * FROM posts WHERE public = 1 ORDER BY date DESC";
+        // $merge = mysqli_query($connect,$query);
+        // $results = mysqli_fetch_all($merge, MYSQLI_ASSOC);
         
-        echo "<script>if (Notification.permission !== 'granted')
-        Notification.requestPermission();</script>";
+        // echo "<script>if (Notification.permission !== 'granted')
+        // Notification.requestPermission();</script>";
         
-        $this->data["posts"] = $results;
+        // $this->data["posts"] = $results;
 
     }
 
