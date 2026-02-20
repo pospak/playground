614f2cf42f5ccdc2ef46400818dfae28bf205f3f
pospak <pospk65@gmail.com>
2026-02-20 17:10:32 +0100

    move to top level of url for testing

--- controlers/UserControler.php
+++ controlers/UserControler.php
@@ -10,16 +10,15 @@ class UserControler extends Controler
         $query->execute();
         $get = $query->get_result();
         $result = $get->fetch_assoc();
-        if ($parameters[0] == "osobni-informace-studenta" && $parameters[1] == "prehled") {
-            $this->view = "overview";
-            $this->head = array(
-                "title" => "Osobní informace studenta",
-                "description" => "Přehled uložených informací",
-                "keywords"  => "Overview, přehled, profil"
-            );
-            if ($result) {
-                $this->data["data"] = $result;
-            }
+
+        $this->view = "overview";
+        $this->head = array(
+            "title" => "Osobní informace studenta",
+            "description" => "Přehled uložených informací",
+            "keywords"  => "Overview, přehled, profil"
+        );
+        if ($result) {
+            $this->data["data"] = $result;
         }
     }
 }
