07c917bc37abc253070a91a8c71eb77a7f4a9d78
pospak <pospk65@gmail.com>
2026-02-20 17:23:08 +0100

    return to previous url

--- controlers/UserControler.php
+++ controlers/UserControler.php
@@ -11,16 +11,16 @@ class UserControler extends Controler
         $query->execute();
         $get = $query->get_result();
         $result = $get->fetch_assoc();
-
-
-        $this->head = array(
-            "title" => "Osobní informace studenta",
-            "description" => "Přehled uložených informací",
-            "keywords"  => "Overview, přehled, profil"
-        );
-        if ($result) {
-            $this->data["data"] = $result;
+        if ($parameters[0] == "osobni-informace-studenta" && $parameters[1] == "prehled") {
+            $this->view = "overview";
+            $this->head = array(
+                "title" => "Osobní informace studenta",
+                "description" => "Přehled uložených informací",
+                "keywords"  => "Overview, přehled, profil"
+            );
+            if ($result) {
+                $this->data["data"] = $result;
+            }
         }
-        $this->view = "overview";
     }
 }
